6d04c3babc705396c4860df05440b4040206872c
Lori Timmons <lorietimmons@gmail.com>
2021-09-09 12:35:17 -0500

    add ability to submit custom tasts

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,15 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name = 'task-name']").value;
   var taskTypeInput = document.querySelector("select[name = 'task-type']").value;
 
+// code validation: check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+  alert("You need to fill out the task form!");
+  return false;
+}
+
+// Will reset the fields in the function 
+formEl.reset(); 
+
   // package up data as an object 
   var taskDataObj = {
     name: taskNameInput,
